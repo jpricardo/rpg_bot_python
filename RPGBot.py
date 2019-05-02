cfe58fb495887af18a567b6a91dd732fa3351fcb
JP <j218738@dac.unicamp.br>
2019-05-02 17:05:17 -0300

    Fight and Run OK, dice are OK, damage calc OK, not stat sensitive

--- RPGBot.py
+++ RPGBot.py
@@ -16,7 +16,6 @@ def command_list():
     comandos = ['dice', 'combat']
     print('Comandos válidos:', comandos)
 
-
 def dice():
     D6 = list(range(1, 7))
     D20 = list(range(1, 21))
@@ -76,16 +75,29 @@ def combat_dice(damage):
             damage.append(dado_roll)
             dado_rep = dado_rep - 1
     if dado_rep == 0:
-        print('Pronto!')
+        print('...')
 
-
-def combat(char_hp, foe_hp, att_stat):
-    damage = []
+def combat(char_hp, foe_hp, att_stat): #MODO DE COMBATE
+    damage = [] #INICIA A LISTA DE DANO
     while char_hp > 0 and foe_hp > 0:
         action = input('O que você vai fazer? (fight, run)').lower()
         if action == 'fight':
-            combat_dice(damage)
-            print('O dano causado foi', damage[0])
+            combat_dice(damage) #CHAMA A FUNÇÃO QUE RANDOMIZA O DANO BASEADO NOS DADOS
+            for i in damage: #SOMA TODOS OS VALORES AO PRIMEIRO
+                if i == damage[0]:
+                    print('CALCULANDO DANO...')
+                else:
+                    damage[0] = damage[0] + i
+            print('O dano causado foi', damage[0]) #MOSTRA O PRIMEIRO VALOR
+        if action == 'run':
+            run_dice = list(range(1, 7))
+            chance = random.choice(run_dice) + random.choice(run_dice)
+            chance = chance/2
+            if chance > 2.5:
+                print('Você fugiu!')
+                break
+            else:
+                print('Não conseguiu fugir!!!')
 
 def functions(choice):
     if choice == 'dice':
